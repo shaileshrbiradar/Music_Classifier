075aad1a00cad4b3602ff8c6133971bb5d3fb282
chetanzope <chetanzope50@gmail.com>
2019-03-15 02:28:23 -0400

    t

--- src/test/java/com/music/tests/UploadFileAndFindGenere.java
+++ src/test/java/com/music/tests/UploadFileAndFindGenere.java
@@ -23,12 +23,19 @@ public class UploadFileAndFindGenere {
 		if(System.getProperty("os.name").toLowerCase().contains("windows")){
 			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/"+"chromedriver.exe");
 		}else if(System.getProperty("os.name").toLowerCase().contains("linux")){
-			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/"+"chromedriver");
+			System.setProperty("webdriver.chrome.driver", "/usr/bin/"+"chromedriver");
+			//System.getProperty("user.dir")+"/"+"chromedriver"
 		}
-		ChromeOptions options = new ChromeOptions();
-		//options.addArguments("--no-sandbox");
-		options.setBinary("/usr/bin/google-chrome");
-		driver = new ChromeDriver(options);
+		/*ChromeOptions options = new ChromeOptions();
+		options.addArguments("start-maximized"); // open Browser in maximized mode
+		options.addArguments("disable-infobars"); // disabling infobars
+		options.addArguments("--disable-extensions"); // disabling extensions
+		options.addArguments("--disable-gpu"); // applicable to windows os only
+		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+		options.addArguments("--no-sandbox"); // Bypass OS security model
+		*/
+		//options.setBinary("/usr/bin/google-chrome");
+		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("http://localhost:8001/");
 		System.out.println("Title of the page : "+driver.getTitle());
